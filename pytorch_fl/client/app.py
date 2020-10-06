3cb074822d90b4c97baa78952e9baff81230bc0e
Gabriel <g.ferrate.c@gmail.com>
2020-10-06 18:09:06 +0100

    WIP

---
+++ pytorch_fl/client/app.py
@@ -0,0 +1,90 @@
+import argparse
+from flask import Flask, request
+import json
+import requests
+import ast
+from client import Client
+
+parser = argparse.ArgumentParser(description='PyTorch FL MNIST Example')
+parser.add_argument('-n', '--num-clients', type=int, metavar='N', required=True,
+                    help='Total number of clients')
+parser.add_argument('-p', '--port', type=str,
+                    help='Client port. Example: 8001')
+parser.add_argument('--batch-size', type=int, default=32, metavar='N',
+                    help='input batch size for training (default: 32)')
+parser.add_argument('-s', '--secure-agg-port', type=int, metavar='N',
+                    help='Secure aggregator port. Example: 8003')
+parser.add_argument('-m', '--main-server-port', type=int, metavar='N',
+                    help='Main server port. Example: 8000')
+
+use_cuda = True
+args = parser.parse_args()
+secure_agg_port = args.secure_agg_port
+main_server_port = args.main_server_port
+client = Client(args.port, args.num_clients, use_cuda)
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return '<h1>{}</h1>'.format(client.client_id)
+
+
+@app.route('/sendstatus', methods=['GET'])
+def send_status():
+    api_url = 'http://localhost:{}/clientstatus'.format(main_server_port)
+    data = {'client_id': client.client_id}
+    print(data)
+    r = requests.post(url=api_url, json=data)
+    print(r, r.status_code, r.reason, r.text)
+    if r.status_code == 200:
+        print("Yeah")
+
+    return "Status OK sent!"
+
+
+@app.route('/sendmodel')
+def send_model():
+    file = open(client.get_model_location(), 'rb')
+    data = {
+        'fname': client.get_model_location(),
+        'id':'http://localhost:{}/'.format(client.port)
+    }
+    files = {
+        'json': ('json_data', json.dumps(data), 'application/json'),
+        'model': ('model1.npy', file, 'application/octet-stream')
+    }
+
+    req = requests.post(
+        url='http://localhost:{}/cmodel'.format(secure_agg_port),
+        files=files
+    )
+    # print(req.text)
+    return "Model sent!"
+
+
+@app.route('/aggmodel', methods=['POST'])
+def get_agg_model():
+    if request.method == 'POST':
+        file = request.files['model'].read()
+        fname = request.files['json'].read()
+
+        fname = ast.literal_eval(fname.decode("utf-8"))
+        fname = fname['fname']
+        print(fname)
+
+        wfile = open("model_update/"+fname, 'wb')
+        wfile.write(file)
+
+        return "Model received!"
+    else:
+        return "No file received!"
+
+
+@app.route('/modeltrain')
+def model_train():
+    train()
+    return "Model trained successfully!"
+
+app.run(host='localhost', port=client.port, debug=False, use_reloader=True)
---
+++ pytorch_fl/client/client.py
@@ -0,0 +1,82 @@
+import torch
+import torch.optim as optim
+from model import Model, get_loaders
+import torch.nn.functional as F
+from torch.optim.lr_scheduler import StepLR
+
+lr = 1.0
+log_interval = 10
+dry_run = True
+gamma = 0.7
+epochs = 10
+save_model = True
+
+
+class Client:
+
+    def __init__(self, port, num_clients, use_cuda=True):
+        self.port = port
+        self.client_id = 'client_{}'.format(self.port)
+        torch.manual_seed(1)
+        use_cuda = use_cuda and torch.cuda.is_available()
+        if not use_cuda:
+            print('\nWARNING: Running without GPU acceleration\n')
+        self.device = torch.device("cuda" if use_cuda else "cpu")
+        self.train_loader, self.test_loader = get_loaders(
+            num_clients, use_cuda)
+        self.model = Model().to(self.device)
+        self.optimizer = optim.Adadelta(self.model.parameters(), lr=lr)
+
+    def train_epoch(self, epoch):
+        self.model.train()
+        for batch_idx, (data, target) in enumerate(self.train_loader):
+            data, target = data.to(self.device), target.to(self.device)
+            self.optimizer.zero_grad()
+            output = self.model(data)
+            loss = F.nll_loss(output, target)
+            loss.backward()
+            self.optimizer.step()
+            if batch_idx % log_interval == 0:
+                print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                    epoch, batch_idx * len(data), len(self.train_loader.dataset),
+                    100. * batch_idx / len(self.train_loader), loss.item()))
+                if dry_run:
+                    break
+
+    def test(self):
+        self.model.eval()
+        test_loss = 0
+        correct = 0
+        with torch.no_grad():
+            for data, target in self.test_loader:
+                data, target = data.to(self.device), target.to(self.device)
+                output = self.model(data)
+                # sum up batch loss
+                test_loss += F.nll_loss(output, target, reduction='sum').item()
+                # get the index of the max log-probability
+                pred = output.argmax(dim=1, keepdim=True)
+                correct += pred.eq(target.view_as(pred)).sum().item()
+
+        test_loss /= len(self.test_loader.dataset)
+
+        print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+            test_loss, correct, len(self.test_loader.dataset),
+            100. * correct / len(self.test_loader.dataset)))
+
+    def get_model_location(self):
+        return 'model_{}.tar'.format(self.client_id)
+
+    def save_model(self):
+        filename = self.get_model_location()
+        torch.save(self.model.state_dict(), filename)
+
+    def train(self):
+        scheduler = StepLR(self.optimizer, step_size=1, gamma=gamma)
+        for epoch in range(1, epochs + 1):
+            self.train_epoch(epoch)
+            self.test()
+            scheduler.step()
+
+        if save_model:
+            self.save_model()
+
