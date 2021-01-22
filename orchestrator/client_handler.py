9db1c044c89170e1bd1e852f10a7394add54197b
Gabriel <g.ferrate.c@gmail.com>
2021-01-22 17:40:23 +0100

    Multithreading redone with Process

--- orchestrator/client_handler.py
+++ orchestrator/client_handler.py
@@ -1,4 +1,4 @@
-from multiprocessing import Process
+from multiprocessing import Process, Manager, Lock
 import requests
 from time import time, sleep
 import logging
@@ -15,7 +15,6 @@ class ClientHandler:
         self.clients = self.parse_clients(clients)
         self.n_clients = len(clients)
         self.OPERATION_MODE = OPERATION_MODE
-        self.processes = []
         logging.info(
             '[Client Handler] Operation mode: {}'.format(self.OPERATION_MODE))
         default_n_firsts = max(1, self.n_clients - 2)
@@ -35,26 +34,30 @@ class ClientHandler:
                          'until {} clients'.format(self.N_FIRSTS))
         else:
             raise Exception('Operation mode not accepted')
-        self.init_operations_history()
+        self.manager = Manager()
+        self.lock = Lock()
+        self.last_operation = self.manager.dict()
+        self.reset_last_operation()
+        self.processes = []
 
     def perform_requests_and_wait(self, endpoint):
         self.perform_parallel_requests(endpoint)
         if self.OPERATION_MODE == 'n_firsts':
             if endpoint == 'send_model':
                 # TODO: Do this part with redundancy
-                return self.wait_until_n_responses(wait_all=True)
-            return self.wait_until_n_responses()
+                return self.wait_until('n_responses', wait_all=True)
+            return self.wait_until('n_responses')
         elif self.OPERATION_MODE == 'timeout':
             self.started = time()
-            return self.wait_until_timeout()
+            return self.wait_until('timeout')
         elif self.OPERATION_MODE == 'wait_all':
-            return self.wait_until_n_responses(wait_all=True)
+            return self.wait_until('n_responses', wait_all=True)
 
-    def init_operations_history(self):
-        self.operations_history = {}
-        for host, port in self.clients:
-            key = self.get_client_key(host, port)
-            self.operations_history[key] = []
+    def reset_last_operation(self):
+        self.lock.acquire()
+        for key in self.clients:
+            self.last_operation[key] = None
+        self.lock.release()
 
     @staticmethod
     def parse_clients(clients):
@@ -66,59 +69,23 @@ class ClientHandler:
         return p_clients
 
     def perform_parallel_requests(self, endpoint):
-        self.init_operations_history()
+        self.reset_last_operation()
         for host, port in self.clients:
             p = Process(target=self.perform_request,
                         args=(host, port, endpoint))
             p.start()
             self.processes.append(p)
 
-    def wait_until_timeout(self):
-        ended_clients = set()
-        completed = False
-        while not completed:
-            for key in self.clients:
-                try:
-                    last_operation = self.operations_history[key][-1]
-                except IndexError:
-                    # Last operation still not computed
-                    continue
-                if key in ended_clients:
-                    continue
-                elif last_operation['ended']:
-                    # TODO: Handle exception when status code != 200
-                    if last_operation['response'].status_code != 200:
-                        raise Exception(
-                            'Error in response. Error code: {}'.format(
-                                last_operation['response'].text))
-                    logging.info(
-                        '[Client Handler] client {} '
-                        'finished performing operation {}'.format(
-                            key, last_operation['op']
-                        )
-                    )
-                    ended_clients.add(key)
-            elapsed = time() - self.started
-            if ((len(ended_clients) >= self.WAIT_FROM_N_FIRSTS) and
-                    elapsed > self.TIMEOUT):
-                completed = True
-            sleep(0.1)
-        self.terminate_processes()
-        return list(ended_clients)
-
-    def wait_until_n_responses(self, wait_all=False):
+    def wait_until(self, until_cond, wait_all=False):
         # TODO: What to do in send model?
         ended_clients = set()
         completed = False
         while not completed:
             # Periodically check if the requests are ending
             for key in self.clients:
-                try:
-                    last_operation = self.operations_history[key][-1]
-                except IndexError:
-                    # Last operation still not computed
-                    continue
-                if key in ended_clients:
+                last_operation = self.last_operation[key]
+                if not last_operation or key in ended_clients:
+                    # Last operation still not computed or already finished
                     continue
                 elif last_operation['ended']:
                     # TODO: Handle exception when status code != 200
@@ -133,16 +100,24 @@ class ClientHandler:
                         )
                     )
                     ended_clients.add(key)
-            if ((not wait_all and (len(ended_clients) >= self.N_FIRSTS))
-                    or (wait_all and len(ended_clients) == self.N_FIRSTS)):
-                completed = True
-            sleep(0.1)
+            if until_cond == 'n_responses':
+                if ((not wait_all and (len(ended_clients) >= self.N_FIRSTS))
+                        or (wait_all and len(ended_clients) == self.N_FIRSTS)):
+                    completed = True
+            elif until_cond == 'timeout':
+                elapsed = time() - self.started
+                if ((len(ended_clients) >= self.WAIT_FROM_N_FIRSTS) and
+                        elapsed > self.TIMEOUT):
+                    completed = True
+            else:
+                raise Exception('Not implemented')
+            sleep(0.2)
         self.terminate_processes()
         return list(ended_clients)
 
     def terminate_processes(self):
         for p in self.processes:
-            p.join()
+            p.terminate()
         self.processes = []
 
     @staticmethod
@@ -150,7 +125,7 @@ class ClientHandler:
         return (host, port)
 
     def perform_request(self, host, port, endpoint):
-        key = self.get_client_key(host, port)
+        client_key = self.get_client_key(host, port)
         last_operation = {
             'started': time(),
             'op': endpoint,
@@ -162,5 +137,7 @@ class ClientHandler:
         last_operation.update({'status': 'ended',
                                'ended': time(),
                                'response': res})
-        self.operations_history.setdefault(key, []).append(last_operation)
+        self.lock.acquire()
+        self.last_operation[client_key] = last_operation
+        self.lock.release()
 
