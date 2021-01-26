c780fbbd811bae7dadc84b9cdca1fb6e77ae33a8
Gabriel <g.ferrate.c@gmail.com>
2021-01-26 19:08:34 +0100

    9 clients iid 7 frames

---
+++ results/9_clients_1_frame/run.py
@@ -0,0 +1,80 @@
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
+import sys
+sys.path.insert(0,'..')
+
+
+
+MARKER_SIZE=1
+
+# Init
+plt.figure()
+
+#epochs = 50
+## Add vertical Lines
+#for i in range(epochs):
+#    plt.axvline(
+#        i+1, linewidth=0.5, color='black', linestyle='dotted', alpha=0.5)
+
+# No FL results
+from non_fedlearn_1_frame.raw_non_fedlearn_1_frame_200 import results
+types = ['test-top1', 'test-top3']
+legend = {'test-top1': 'no-fl-top-1', 'test-top3': 'no-fl-top-3'}
+### IMPORTANT!!!!!!! LAST EPOCH IS LAST TEST WITH BEST RESULT AND CLUSTERING
+epochs = len(results) - 1
+X_AXIS = list(map(lambda x: x+1, range(epochs)))
+for t in types:
+    y_values = [(e, x[t]) for e, x in results.items()][:-1]
+    y_values = list(sorted(y_values, key=lambda t: t[0])) # Order by epoch asc
+    y_values = [x[1] for x in y_values]
+    plt.plot(X_AXIS, y_values, linewidth=1, label=legend[t], marker='o', markersize=MARKER_SIZE)
+    plt.legend(loc="upper right")
+
+# IID results:
+from raw_9_clients_iid_1_frames import results
+epochs = len(results)
+X_AXIS = list(map(lambda x: x+1, range(epochs)))
+types = ['test-top1', 'test-top3']
+legend = {'test-top1': '9-clients-iid-top-1',
+          'test-top3': '9-clients-iid-top-3'}
+for t in types:
+    y_values = [x['test_result'][t] for x in results]
+    plt.plot(X_AXIS,
+             y_values,
+             linewidth=1,
+             label=legend[t],
+             linestyle='solid',
+             marker="^",
+             markersize=MARKER_SIZE)
+    plt.legend(loc="upper right")
+
+# NON-IID results:
+from raw_9_clients_non_iid_1_frames import results
+epochs = len(results)
+X_AXIS = list(map(lambda x: x+1, range(epochs)))
+types = ['test-top1', 'test-top3']
+legend = {'test-top1': '9-clients-non-iid-top-1',
+          'test-top3': '9-clients-non-iid-top-3'}
+for t in types:
+    y_values = [x['test_result'][t] for x in results]
+    plt.plot(X_AXIS,
+             y_values,
+             linewidth=1,
+             label=legend[t],
+             linestyle='solid',
+             marker="s",
+             markersize=MARKER_SIZE)
+    plt.legend(loc="upper right")
+
+# Limits
+epochs = 200
+plt.axis(xmin=0, xmax=epochs+1, ymin=0, ymax=100)
+
+# Labels
+#plt.title('1 input frame comparison with 9 clients')
+plt.ylabel('Accuracy')
+plt.xlabel('Epoch / Communication Round')
+
+# Save
+plt.savefig('result.png', dpi=400)
---
+++ results/non_fedlearn_1_frame/extract_results.py
@@ -0,0 +1,48 @@
+
+
+def extract_results(fn):
+    data = {}
+    clustering = False
+    with open(fn, 'r') as f:
+        for l in f:
+            if 'clustering...' in l:
+                clustering = True
+            if l.startswith('Test'):
+                s = l.split('\t')
+                epoch = int(s[0].split('[')[1].split(']')[0])
+                top_1 = float(s[-2].split(' ')[1].split(' ')[0])
+                top_3 = float(s[-1].split(' ')[1].split(' ')[0])
+                time = float(s[1].split(' ')[1].split(' ')[0])
+                if clustering:
+                    data.setdefault(epoch, {}).setdefault('test-top1-clustering', []).append(top_1)
+                    data[epoch].setdefault('test-top3-clustering', []).append(top_3)
+                else:
+                    data.setdefault(epoch, {}).setdefault('test-top1', []).append(top_1)
+                    data[epoch].setdefault('test-top3', []).append(top_3)
+            if l.startswith('Test') or l.startswith('Train'):
+                s = l.split('\t')
+                epoch = int(s[0].split('[')[1].split(']')[0])
+                time = float(s[1].split(' ')[1].split(' ')[0])
+                data.setdefault(epoch, {}).setdefault('time', 0.0)
+                data[epoch]['time'] += time
+    return data
+
+def avg(_list):
+    return sum(_list) / len(_list)
+
+def average_epochs(data):
+    for epoch, d in data.items():
+        d['test-top1'] = avg(d['test-top1'])
+        d['test-top3'] = avg(d['test-top3'])
+        if 'test-top1-clustering' in d:
+            d['test-top1-clustering'] = avg(d['test-top1-clustering'])
+            d['test-top3-clustering'] = avg(d['test-top3-clustering'])
+    return data
+
+if __name__ == '__main__':
+    fn = '1_frames_non_fedlearn_200_epochs.log'
+    data = extract_results(fn)
+    average_epochs(data)
+    print(data)
+
+
